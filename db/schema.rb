64b52eadecfc2cdcc015077ea4d956dd9d396bb2
Michael Sutton <hicallmesutton@gmail.com>
2021-08-01 22:50:57 -0500

    converted to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_06_202440) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: :cascade do |t|
     t.integer "user_id"
     t.integer "level"
